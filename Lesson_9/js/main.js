32d26c58597dbf2b2690c944ca804496f56c1044
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-03-15 11:42:05 +0100

    Задание по this

---
+++ Lesson_9/js/main.js
@@ -0,0 +1,9 @@
+let age = document.getElementById('age');
+
+function showUser(surname, name) {
+
+  alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+
+}
+
+showUser.call(age, 'Ещенко', 'Станислав');
